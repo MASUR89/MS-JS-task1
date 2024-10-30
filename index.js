476aac6575d9aae7c0953f1884846750cca73aeb
MASUR89 <surmava9@gmail.com>
2024-10-30 19:11:08 +0400

    zz

---
+++ index.js
@@ -0,0 +1,39 @@
+let Name1 = "Giorgi"
+let Surname1 = "Giorgadze"
+let Age1 = 22
+let Email1 = "giorgadze@gmail.com"
+let MobN1 = 599111222
+let isAdult1 = true
+console.log("P1 personal data: Name -", Name1, ", surname -", Surname1, ", age -", Age1, ", Email -", Email1, ", telephone # -", MobN1, ", adult:", "%c color:yellow ",isAdult1, );
+
+let Name2 = "Nikoloz"
+let Surname2 = "Nikachadze"
+let Age2 = 12
+let Email2 = "nikoloz@gmail.com"
+let MobN2 = 599222333
+let isAdult2 = false
+console.log("P2 personal data: Name -", Name2, ", surname -", Surname2, ", age -", Age2, ", Email -", Email2, ", telephone # -", MobN2, ", adult:", isAdult2);
+
+let Name3 = "Mariam"
+let Surname3 = "Mariamidze"
+let Age3 = 30
+let Email3 = "mariamidze@gmail.com"
+let MobN3 = 599333444
+let isAdult3 = true
+console.log("P3 personal data: Name -", Name3, ", surname -", Surname3, ", age -", Age3, ", Email -", Email3, ", telephone # -", MobN3, ", adult:", isAdult3);
+
+let Name4 = "Nino"
+let Surname4 = "Ninidze"
+let Age4 = 17
+let Email4 = "ninidze@gmail.com"
+let MobN4 = 599444555
+let isAdult4 = false
+console.log("P4 personal data: Name -", Name4, ", surname -", Surname4, ", age -", Age4, ", Email -", Email4, ", telephone # -", MobN4, ", adult:", isAdult4);
+
+let Name5 = "Dachi"
+let Surname5 = "Papava"
+let Age5 = 25
+let Email5 = "papava@gmail.com"
+let MobN5 = 599555666
+let isAdult5 = true
+console.log("P5 personal data: Name -", Name5, ", surname -", Surname5, ", age -", Age5, ", Email -", Email5, ", telephone # -", MobN5, ", adult:", isAdult5);
