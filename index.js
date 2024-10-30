5ad5f50bd58e6bcaac912abec8299a6577fdf7ab
MASUR89 <surmava9@gmail.com>
2024-10-30 22:11:17 +0400

    ჯს1

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ let Age1 = 22
 let Email1 = "giorgadze@gmail.com"
 let MobN1 = 599111222
 let isAdult1 = true
-console.log("P1 personal data: Name -", Name1, ", surname -", Surname1, ", age -", Age1, ", Email -", Email1, ", telephone # -", MobN1, ", adult:", "%c color:yellow ",isAdult1, );
+console.log(
+    "P1 personal data: Name -", Name1,
+    ", surname -", Surname1,
+    ", age -", Age1,
+    ", Email -", Email1,  );
+console.log("Ttelephone number: %c" + MobN1, "color: red")
+console.log("Adult: %c" + isAdult1, "color: orange");
+
+
 
 let Name2 = "Nikoloz"
 let Surname2 = "Nikachadze"
@@ -12,7 +20,13 @@ let Age2 = 12
 let Email2 = "nikoloz@gmail.com"
 let MobN2 = 599222333
 let isAdult2 = false
-console.log("P2 personal data: Name -", Name2, ", surname -", Surname2, ", age -", Age2, ", Email -", Email2, ", telephone # -", MobN2, ", adult:", isAdult2);
+console.log(
+    "P2 personal data: Name -", Name2, 
+    ", surname -", Surname2, 
+    ", age -", Age2, 
+    ", Email -", Email2, );
+console.log("Ttelephone number: %c" + MobN2, "color: red")
+console.log("Adult: %c" + isAdult2, "color: orange");
 
 let Name3 = "Mariam"
 let Surname3 = "Mariamidze"
@@ -20,7 +34,9 @@ let Age3 = 30
 let Email3 = "mariamidze@gmail.com"
 let MobN3 = 599333444
 let isAdult3 = true
-console.log("P3 personal data: Name -", Name3, ", surname -", Surname3, ", age -", Age3, ", Email -", Email3, ", telephone # -", MobN3, ", adult:", isAdult3);
+console.log("P3 personal data: Name -", Name3, ", surname -", Surname3, ", age -", Age3, ", Email -", Email3,);
+console.log("Ttelephone number: %c" + MobN3, "color: red")
+console.log("Adult: %c" + isAdult3, "color: orange");
 
 let Name4 = "Nino"
 let Surname4 = "Ninidze"
@@ -28,7 +44,9 @@ let Age4 = 17
 let Email4 = "ninidze@gmail.com"
 let MobN4 = 599444555
 let isAdult4 = false
-console.log("P4 personal data: Name -", Name4, ", surname -", Surname4, ", age -", Age4, ", Email -", Email4, ", telephone # -", MobN4, ", adult:", isAdult4);
+console.log("P4 personal data: Name -", Name4, ", surname -", Surname4, ", age -", Age4, ", Email -", Email4, );
+console.log("Ttelephone number: %c" + MobN4, "color: red")
+console.log("Adult: %c" + isAdult4, "color: orange");
 
 let Name5 = "Dachi"
 let Surname5 = "Papava"
@@ -36,4 +54,8 @@ let Age5 = 25
 let Email5 = "papava@gmail.com"
 let MobN5 = 599555666
 let isAdult5 = true
-console.log("P5 personal data: Name -", Name5, ", surname -", Surname5, ", age -", Age5, ", Email -", Email5, ", telephone # -", MobN5, ", adult:", isAdult5);
+console.log("P5 personal data: Name -", Name5, ", surname -", Surname5, ", age -", Age5, ", Email -", Email5);
+console.log("Ttelephone number: %c" + MobN5, "color: red")
+console.log("Adult: %c" + isAdult5, "color: orange");
+
+// მხოლოდ ასე მოვახერხე
